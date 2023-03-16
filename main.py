f8258aa45013006043efd26dbe06da45dd79d419
Deepak_Baskar <91734673+Deepakbaskar94@users.noreply.github.com>
2023-03-16 14:20:34 +0530

    updated code

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask
-import logging
+from flask import Flask, render_template
+import flask_monitoringdashboard as dashboard
+
 # Flask constructor takes the name of
 # current module (__name__) as argument.
 app = Flask(__name__)
 
+dashboard.config.init_from(file='/home/frappe/Desktop/small Projects/flask_base/config.cfg')
 
- 
-logging.basicConfig(filename='record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
- 
+dashboard.bind(app)
 
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
 # the associated function.
-@app.route('/')
+@app.route('/base')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    print("this is printed")
-    logging.debug('This is a debug message')
-    logging.info('This is an info message')
-    logging.warning('This is a warning message')
-    logging.error('This is an error message')
-    logging.critical('This is a critical message')
-    return 'Hello World test6'
+	return 'Hello World test6'
+
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 @app.route('/hello/<name>')
 def hello_name(name):
-   logging.info('This is an name value %s', name)
    return 'Hello %s!' % name
 
 
